ca2311a2292044b0562a590db3c1551624da903d
Ashkan Mohammadi <mohammadiashkan1384@gmail.com>
2022-05-27 14:47:38 +0430

    [util] add `write_output` utility

--- data_loader.py
+++ data_loader.py
@@ -6,3 +6,6 @@ def load_data(path: str):
         with open(path, 'r') as data_file: return data_file.read()
     else:
         return None
+
+def write_output(result: str, path):
+    with open(path, 'w') as output_file: output_file.write(result)
