760ba6d5aadf692f0cf97431b12f546bfd60e5fe
Ashkan Mohammadi <mohammadiashkan1384@gmail.com>
2022-05-27 14:48:16 +0430

    [feat] integrate the CLI interface with the transpiler

--- gsdlgen.py
+++ gsdlgen.py
@@ -5,12 +5,14 @@ from pprint import pprint
 # local imports
 from parser import GSDLParser
 from lexer import GSDLLexer
-from data_loader import load_data
+from data_loader import load_data, write_output
 from preprocessor import GSDLPreprocessor
+from transpiler import GSDLTranspiler
 
 
 cli_argument_parser = ArgumentParser("CLI interface to GDSL preprocessor")
-cli_argument_parser.add_argument('input', type=str, help="gsdlgen.py --input ./gql/schema.gdsl",)
+cli_argument_parser.add_argument('input', type=str, help="gsdlgen.py ./gql/schema.gdsl",)
+cli_argument_parser.add_argument('-o', dest="output", type=str, help="gsdlgen.py ./input.gsdl ./gql/schema.graphql", default="./schema.graphql")
 
 args = cli_argument_parser.parse_args()
 
@@ -20,9 +22,12 @@ if __name__ == '__main__':
         lexer = GSDLLexer()
         parser = GSDLParser()
         preprocessor = GSDLPreprocessor()
+        transpiler = GSDLTranspiler()
         parse_tree = parser.parse(lexer.tokenize(gsdl_schema))
-        parsed = preprocessor.preprocess(parse_tree)
-        pprint(parsed)
+        processed_parse_tree = preprocessor.preprocess(parse_tree)
+        schema_output = transpiler.transpile(processed_parse_tree)
+        write_output(schema_output, args.output)
+        
 
     else:
         print("Couldn't find the path specified, here are some useful information:")
